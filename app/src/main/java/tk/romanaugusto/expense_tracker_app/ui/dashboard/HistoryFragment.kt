5dd2662b360c95258f511071a5a41dd053e44b1d
kyouma <roman.cabalum@gmail.com>
2023-04-21 19:57:44 +0800

    added input view

--- app/src/main/java/tk/romanaugusto/expense_tracker_app/ui/dashboard/HistoryFragment.kt
+++ app/src/main/java/tk/romanaugusto/expense_tracker_app/ui/dashboard/HistoryFragment.kt
@@ -1,23 +1,27 @@
 package tk.romanaugusto.expense_tracker_app.ui.dashboard
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.core.view.setMargins
 import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.mashape.unirest.http.HttpResponse
 import com.mashape.unirest.http.Unirest
 import kotlinx.coroutines.*
+import org.json.JSONArray
 import org.json.JSONObject
 import tk.romanaugusto.expense_tracker_app.R
 import tk.romanaugusto.expense_tracker_app.databinding.FragmentHistoryBinding
 import java.io.File
+import java.util.*
 
 class HistoryFragment : Fragment() {
     private val urlString = "http://10.178.24.180:8080/expenses/all"
@@ -51,33 +55,62 @@ class HistoryFragment : Fragment() {
         requestScope.launch {
            historyListOfJSON = getHistory(userData.optString("_id"))
         }.invokeOnCompletion {
-            val baseList = mutableListOf<JSONObject>()
-            val list  = JSONObject(historyListOfJSON.body)
-                .getJSONArray("expenses")
-            for (json in 0 until list.length()) {
-                baseList.add(list.getJSONObject(json))
-            }
-            baseList.forEach {
-                val textView = TextView(this.context).apply {
-                    this.setPadding(5)
-                }
-                val textString = it.getString("title") + " " + it.getString("integer_symbol") + it.getString("amount")
-                println(textString)
-                textView.text = textString
-
-                mainScope.launch {
-                    layOut.addView(textView)
-                }
-            }
+            addRecordsToLayout(layOut)
         }
 
         return root
+    }
+
+    private fun addRecordsToLayout (outerView:LinearLayout) {
+        val baseList = mutableListOf<JSONObject>()
+        val list  = JSONObject(historyListOfJSON.body)
+            .getJSONArray("expenses")
+        for (json in 0 until list.length()) {
+            baseList.add(list.getJSONObject(json))
+        }
+
+        var counter = 0
+        baseList.forEach {
+            val displayMetrics = resources.displayMetrics
+            val layout = LinearLayout(this.context).apply {
+                this.layoutParams = LinearLayout.LayoutParams(displayMetrics.widthPixels,LinearLayout.LayoutParams.WRAP_CONTENT).apply {
+                    this.setMargins(12)
+                }
+            }
+
+            layout.setPadding(50)
+            layout.orientation = LinearLayout.HORIZONTAL
+            layout.gravity = Gravity.START
+            layout.setPadding(60,0,60,0)
+
+            val titleView = TextView(this.context)
+            titleView.text = it.getString("title")
+                .replaceFirstChar {
+                    if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+                }
+            titleView.layoutParams =
+                LinearLayout.LayoutParams((displayMetrics.widthPixels * .70).toInt(),LinearLayout.LayoutParams.WRAP_CONTENT)
+
+            val priceView = TextView(this.context)
+            val priceString = it.getString("integer_symbol") + it.getString("amount")
+            priceView.text = priceString
+
+
+            layout.addView(titleView)
+            layout.addView(priceView)
+
+            mainScope.launch {
+                outerView.addView(layout)
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
     private fun getHistory (userId:String): HttpResponse<String> = Unirest.post(urlString)
         .header("accept", "*/*")
         .header("Content-Type", "application/json")
